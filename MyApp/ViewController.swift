28419507f8adfea579d63b6c0ca3b2ed6cc8d176
Nicolas Christe <nicolas.christe@tagheuer.com>
2018-08-10 16:36:33 +0200

    Add stringdict

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let s = String.localizedStringWithFormat(NSLocalizedString("Book a room for %d nights", comment: "book a room"), 1)
+        print(s)
     }
 
     override func didReceiveMemoryWarning() {
